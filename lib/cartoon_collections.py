bb47a4b873ac09e956dfa1313d74a981daf0c94f
bill <bill.arnold@student.moringaschool.com>
2023-11-23 22:33:27 +0300

    complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+# lib/dwarves.py
 
-def summon_captain_planet():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, 1):
+        print(f"{i}. {dwarf}")
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + '!' for call in planeteer_calls]
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
+
+def find_the_cheese(snacks):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for item in snacks:
+        if item in cheeses:
+            return item
+    return None
